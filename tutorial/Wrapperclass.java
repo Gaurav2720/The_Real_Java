c20020f3f0163ada0e5c951c89455f2ede0ce575
Gaurav Bhaskar <iamgaurav2702@gmail.com>
2026-05-09 21:09:40 +0530

    adding wrapper class its easy two main concept of autoocing and unboxing are clear now

---
+++ tutorial/Wrapperclass.java
@@ -0,0 +1,18 @@
+public class Wrapperclass{
+
+    public static void main(String[] args) {
+        
+        String obj = new String("12");
+        Integer obj2 = Integer.valueOf(20);
+
+        System.out.println(obj2);
+        System.out.println(obj);
+
+        
+
+        String age = obj;
+        //System.out.println(obj.TYPE);
+
+    }
+
+}
