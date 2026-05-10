8d72aef9cabec31240a11068c97ac90c9ddddd19
Gaurav Bhaskar <iamgaurav2702@gmail.com>
2026-05-10 22:37:20 +0530

    adding gemeric

---
+++ tutorial/Generic.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class input<T> {
+T inputcomponents;
+
+public input(T inputcomponents){
+
+    this.inputcomponents = inputcomponents;
+
+}
+public void temperature(){
+
+    System.out.println("enter the temperature in celcius ");
+    T<Integer> = sc
+
+}
+}
+
+public class Generic {
+    Scanner sc = new Scanner(System.in);
+
+}
