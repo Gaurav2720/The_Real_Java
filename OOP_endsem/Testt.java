94480385afc33e041112acc1191d0121d7ba377e
Gaurav Bhaskar <iamgaurav2702@gmail.com>
2026-05-11 09:54:10 +0530

    adding more code

--- OOP_endsem/Testt.java
+++ OOP_endsem/Testt.java
@@ -1,13 +1,16 @@
 public class Testt {
     public static void main(String[] args) {
         
-        String mystring = "hello";
-        mystring.concat("my name is gaurav");
+        String mystring = "my name is gaurav";
 
         System.out.println(mystring);
 
-
-        String myString = "bhak bsdk";
+        String[] stringarray = mystring.split(" ");
+        int a = stringarray.length; // for words but requores split 
+        int b = mystring.length();// for all letter 
+        System.out.println(b);
+        System.out.println(a);
+        System.out.println(stringarray);
         // its immutable 
 
 
