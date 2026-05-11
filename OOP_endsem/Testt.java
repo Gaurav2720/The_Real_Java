4bb97cc2c8f8ee40b3e95b4d4579d8ace1b58360
Gaurav Bhaskar <iamgaurav2702@gmail.com>
2026-05-11 09:48:03 +0530

    strings done

---
+++ OOP_endsem/Testt.java
@@ -0,0 +1,24 @@
+public class Testt {
+    public static void main(String[] args) {
+        
+        String mystring = "hello";
+        mystring.concat("my name is gaurav");
+
+        System.out.println(mystring);
+
+
+        String myString = "bhak bsdk";
+        // its immutable 
+
+
+        StringBuffer S = new StringBuffer("yo wassup!!");
+
+        S.append(("helloo"));
+        
+        System.out.println(S);
+
+        StringBuilder S1 = new StringBuilder("chal bsdk!");
+        System.out.println(S1);
+
+    }
+}
