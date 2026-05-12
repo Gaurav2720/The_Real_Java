8987b297dd1aa7aa258196364113e46399b459ae
Gaurav Bhaskar <iamgaurav2702@gmail.com>
2026-05-12 23:05:49 +0530

    generic ille

---
+++ OOP_endsem/Generic.java
@@ -0,0 +1,15 @@
+
+
+class Unit {
+
+        public static <T extends Number> double convert(T value , String type){
+
+            double val = value.doubleValue();
+                
+        }
+}
+public class Generic {
+
+
+    
+}
